6e21fab73cfb44d7e709338261cab65ce77ebeca
patelmanasi <142138381+patelmanasi@users.noreply.github.com>
2026-08-14 16:49:29 +0530

    Enhance product management with search, advanced filters, pagination and filtered CSV export

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,14 +1,19 @@
 <?php
-// app/Models/Product.php
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\SchemalessAttributes\Casts\SchemalessAttributes;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'description', 'price', 'extra_attributes'];
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'extra_attributes',
+    ];
 
     protected $casts = [
         'extra_attributes' => SchemalessAttributes::class,
